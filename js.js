dab55af6e6931cd3861dcc062972b423b58ec3fc
ElZiga <krutt0610@gmail.com>
2021-12-04 19:57:14 +0200

    dfsdf

--- js.js
+++ js.js
@@ -13,5 +13,30 @@ xhr.onload = function() {
     <h1>${products[0].name} </h1>
     <img width="250px;" src='${products[0].photo_url}'>
     </div>
+
+    <div class = "item">
+    <h1>${products[1].name} </h1>
+    <img    width="250px;" src='${products[1].photo_url}'>
+    </div>
+
+    <div class = "item">
+    <h1>${products[2].name} </h1>
+    <img width="250px;" src='${products[2].photo_url}'>
+    </div>
+
+    <div class = "item">
+    <h1>${products[3].name} </h1>
+    <img width="250px;" src='${products[3].photo_url}'>
+    </div>
+
+    <div class = "item">
+    <h1>${products[4].name} </h1>
+    <img width="250px;" src='${products[4].photo_url}'>
+    </div>
+
+    <div class = "item">
+    <h1>${products[5].name} </h1>
+    <img width="250px;" src='${products[5].photo_url}'>
+    </div>
     `
 }
