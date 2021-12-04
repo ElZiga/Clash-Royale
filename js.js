844ab57a09968630ca7453b58f4edb24708bdf75
ElZiga <krutt0610@gmail.com>
2021-12-04 19:40:12 +0200

    hdh

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+let item = document.getElementById('grid')
 let xhr = new XMLHttpRequest;
 let url = 'https://my-json-server.typicode.com/ElZiga/Clash-Royale';
 xhr.open("GET", url+ "/products/")
@@ -7,9 +8,10 @@ xhr.send()
 
 xhr.onload = function() {
     let products = xhr.response
-    ClipboardItem.innerHTML = `
+    item.innerHTML = `
     <div class = "item">
     <h1>${products[0].name} </h1>
+    <img width="250px;" src='${products[0].photo_url}'>
     </div>
     `
 }
